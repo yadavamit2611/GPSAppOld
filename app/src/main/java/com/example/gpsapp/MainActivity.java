9079ce8b53cc31bb0a8d4dd0943e0dd31ad3798e
ameybenare <amey.benare@gmail.com>
2023-09-13 17:05:23 +0200

    Main Activity Template

--- app/src/main/java/com/example/gpsapp/MainActivity.java
+++ app/src/main/java/com/example/gpsapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,10 +13,27 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     TextView welcome;
+
+    //TextView tvLongitude,tvLatitude, tvAltitude,tvAccuracy,tvSpeed,tvAddress;
+
+    TextView tvLongitudeVal,tvLatitudeVal, tvAltitudeVal,tvAccuracyVal,tvSpeedVal,tvAddressVal,tvUpdates,tvSensor;
+    Switch swGps, swLocationUpdates;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvLongitudeVal = findViewById(R.id.tvLongitudeVal);
+        tvLatitudeVal = findViewById(R.id.tvLatitudeVal);
+        tvAltitudeVal = findViewById(R.id.tvAltitudeVal);
+        tvAccuracyVal = findViewById(R.id.tvAccuracyVal);
+        tvSpeedVal = findViewById(R.id.tvSpeedVal);
+        tvAddressVal = findViewById(R.id.tvAddressVal);
+        tvUpdates = findViewById(R.id.tvUpdates);
+        tvSensor = findViewById(R.id.tvSensor);
+        swGps = findViewById(R.id.sw_gps);
+        swLocationUpdates = findViewById(R.id.sw_locationsupdates);
+
+
         auth = FirebaseAuth.getInstance();
         welcome = findViewById(R.id.welcome);
         if(auth.getCurrentUser() == null){
